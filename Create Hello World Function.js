58a4aa0143dc7bb6bf090f750b3c8596420830f3
V26 <vikasanipeddi26@gmail.com>
2023-09-09 16:54:42 +0530

    "Hello World", bit ambigousos should workon it

---
+++ Create Hello World Function.js
@@ -0,0 +1,31 @@
+
+// var createHelloWorld = function() {
+//     return function() {
+
+//         return "Hello World"
+        
+//     }
+// };
+
+// console.log(createHelloWorld());
+
+
+var sub=(a,b)=>{
+    return a-b;
+}
+
+console.log(sub(6,2));
+
+
+var createHelloWorld=()=>{
+    
+    return "Hello World"
+}
+
+
+
+
+// const createHelloWorld =() =>()=> "Hello World";
+
+
+console.log(createHelloWorld())
